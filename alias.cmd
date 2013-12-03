d3ef936216d99f7f32091915420631efbf59e329
Jacob <jacobaarthur@gmail.com>
2013-12-02 21:29:40 -0600

    Added help to alias and expanded generated cmd to include a simple usage message

--- alias.cmd
+++ alias.cmd
@@ -1,6 +1,46 @@
 @echo off
 
-echo @echo off >> %~1.cmd
-echo %~2 %%1 %%2 %%3 %%4 %%5 %%6 %%7 %%8 %%9 >> %~1.cmd
+if "%1" == "" (
+	goto :HELP
+)
 
+if "%2" == "" (
+	goto :HELP
+)
 
+if "%1"=="-h" (
+	goto :HELP
+)
+if "%1"=="/h" (
+	goto :HELP
+)
+if "%1"=="/?" (
+	goto :HELP
+)
+
+set file=%~1.cmd
+
+echo @echo off >> %file%
+echo if "%%1"=="-h" ( goto :HELP ) >> %file%
+echo if "%%1"=="/h" ( goto :HELP ) >> %file%
+echo if "%%1"=="/?" ( goto :HELP ) >> %file%
+
+echo %~2 %%1 %%2 %%3 %%4 %%5 %%6 %%7 %%8 %%9 >> %file%
+echo goto :EXIT >> %file%
+
+echo :HELP >> %file%
+echo echo %~1 alias for "%~2" >> %file%
+echo :EXIT >> %file% 
+
+goto :EXIT
+
+:HELP
+echo.
+echo 	alias: creates an alias cmd
+echo ----------------------------------------------
+echo	alias [name] ["command"]
+echo		Must give both a name and a command
+echo		for a command with spaces wrap with double quotes
+echo.
+
+:EXIT
